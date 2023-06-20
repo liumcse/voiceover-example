e86f56b6642695d3be2057ece81767c9bc2fed34
Mingyu <xming@google.com>
2023-06-20 19:06:02 +0800

    Initial Commit

---
+++ voiceover-example/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  voiceover-example
+//
+//  Created by Mingyu Liu on 20/6/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ voiceover-example/voiceover_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  voiceover_exampleApp.swift
+//  voiceover-example
+//
+//  Created by Mingyu Liu on 20/6/23.
+//
+
+import SwiftUI
+
+@main
+struct voiceover_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
